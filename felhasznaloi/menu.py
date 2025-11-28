6f564ab3921db482c9fe846a06c62b23abcded5f
Kavefozo <mikolt@mauzer.net>
2025-11-28 21:26:37 +0200

    Add files via upload

--- felhasznaloi/menu.py
+++ felhasznaloi/menu.py
@@ -1,4 +1,5 @@
 import pygame
+pygame.font.init()
 
 screen = pygame.display.set_mode((1000,600))
 pygame.display.set_caption("Damareen")
@@ -11,18 +12,29 @@ def menu():
     global szaml
     global ok
     global oszl
+    global old
     oszl = 3
     ok = True
-    tav = 110
     szaml = 0
     tavle = -70
     screen.fill((195,150,99))
     Pakli_hatter.draw()
     Pakli.draw()
-    for i in kp:
-        for j in k:
-            if j.nev == i:
-                j.draw_pakli(tav)
+    Jobb.draw()
+    if (len(kp)<6)or (6*old>len(kp)):
+        Jobb.clicked = True
+    else:
+        Jobb.clicked = False
+    Bal.draw()
+    if old==1:
+        Bal.clicked = True
+    else:
+        Bal.clicked = False
+    for i in range((old-1)*6,len(kp)):
+        if i<(old-1)*6+6:
+            for j in k:
+                if j.nev == kp[i]:
+                    j.draw_pakli(110)
     Kilepes.draw()
     Harc1.draw()
     Harc2.draw()
@@ -39,7 +51,6 @@ def pakli():
     global oszl
     global ok
     global szaml
-    tav = 60
     tavle = -40
     oszl = 7
     szaml = 0
@@ -57,7 +68,7 @@ def pakli():
     for i in kp:
         for j in k:
             if j.nev == i:
-                j.draw_pakli(tav)
+                j.draw_pakli(60)
 
 def harc1():
     global ii
@@ -100,11 +111,23 @@ def igen():
     global running
     running = False
 
+def jobb_():
+    global old
+    global ii
+    old = old+1
+    ii = 0
+
+def bal_():
+    global old
+    global ii
+    old = old-1
+    ii = 0
+
 ii = 0
-defes_tomb = [menu, pakli, harc1, harc2, harc3, kilepes, igen]
+defes_tomb = [menu, pakli, harc1, harc2, harc3, kilepes, igen, jobb_, bal_]
 
 
-kg = ['1','2','3','4','5','6','7']
+kg = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14']
 kp = []
 szaml = 0
 sorok = 0
@@ -123,6 +146,9 @@ megsem = pygame.image.load("nem.png").convert()
 #megsem.set_colorkey((255,255,255))
 mentes = pygame.image.load("mentes.png").convert()
 
+jobbra = pygame.image.load("nyil_j.png").convert()
+balra = pygame.image.load("nyil_b.png").convert()
+
 #hatterek
 biztos = pygame.image.load("biztos.png").convert()
 pakli_hatter = pygame.image.load("pakli_keret.png").convert()
@@ -136,9 +162,12 @@ kartya4 = pygame.image.load("kartya.png").convert()
 kartya5 = pygame.image.load("kartya.png").convert()
 kartya6 = pygame.image.load("kartya.png").convert()
 kartya7 = pygame.image.load("kartya.png").convert()
+kartya8 = pygame.image.load("kartya.png").convert()
 
 class Kartyak:
-    def __init__(self,x,y,kep,scale,nev):
+    def __init__(self,x,y,kep,scale,nev, sebzes, hp):
+        self.sebzes = sebzes
+        self.hp = hp
         self.clicked = False
         self.mouse = False
         self.pressed = False
@@ -161,6 +190,10 @@ class Kartyak:
         self.x = tav
         self.y = tavle
         screen.blit(self.kep, (self.x, self.y))
+        text_s = font.render(str(self.sebzes), True, (90, 30, 10))
+        text_hp = font.render(str(self.hp), True, (90, 30, 10))
+        screen.blit(text_s, (self.x + 43, self.y + 42))
+        screen.blit(text_hp, (self.x + 43, self.y + 75))
         tav = tav + 100
         if ii == 1:
             self.clicked = False
@@ -195,9 +228,15 @@ class Kartyak:
 
     def draw_gyujt(self):
         global tav2
+        global font
         self.y = 450
         self.x = tav2
         screen.blit(self.kep, (self.x, self.y))
+        text_s = font.render(str(self.sebzes), True, (90, 30, 10))
+        text_hp = font.render(str(self.hp), True, (90, 30, 10))
+        screen.blit(text_s, (self.x + 43, self.y + 42))
+        screen.blit(text_hp, (self.x + 43, self.y + 75))
+
         tav2 = tav2 + 100
 
         pos = pygame.mouse.get_pos()
@@ -303,6 +342,8 @@ Kilepes = Gombok(20, 530, kilepes, 0.9, 'kilepes', 5)
 Igen = Gombok(510, 310, igen, 1, 'igen', 6)
 Megsem = Gombok(340, 310, megsem, 1, 'megsem', 0)
 Mentes = Gombok(400, 350, mentes, 0.8, 'mentes', 0)
+Jobb = Gombok(265,400,jobbra,1,'jobb',7)
+Bal = Gombok(205,400,balra,1,'bal',8)
 
 #hatterek
 Biztos = Ablakok(300,170, biztos,1)
@@ -310,18 +351,23 @@ Pakli_hatter = Ablakok(70, 20, pakli_hatter, 1)
 Uj_pakli_hatter = Ablakok(30,40,uj_pakli_hatter, 1)
 
 #kartyak
-Kartya1 = Kartyak(0,0,kartya1,1,'1')
-Kartya2 = Kartyak(0,0,kartya2,1,'2')
-Kartya3 = Kartyak(0,0,kartya3,1,'3')
-Kartya4 = Kartyak(0,0,kartya4,1,'4')
-Kartya5 = Kartyak(0,0,kartya5,1,'5')
-Kartya6 = Kartyak(0,0,kartya6,1,'6')
-Kartya7 = Kartyak(0,0,kartya7,1,'7')
+Kartya1 = Kartyak(0,0,kartya1,1,'1',100,2)
+Kartya2 = Kartyak(0,0,kartya2,1,'2',4,2)
+Kartya3 = Kartyak(0,0,kartya3,1,'3',2,4)
+Kartya4 = Kartyak(0,0,kartya4,1,'4',3,3)
+Kartya5 = Kartyak(0,0,kartya5,1,'5',2,2)
+Kartya6 = Kartyak(0,0,kartya6,1,'6',3,2)
+Kartya7 = Kartyak(0,0,kartya7,1,'7',2,3)
+Kartya8 = Kartyak(0,0,kartya8,1,'8',12,3)
+Kartya9 = Kartyak(0,0,kartya8,1,'9',3,20)
 
-k = [Kartya1, Kartya2, Kartya3, Kartya4, Kartya5, Kartya6, Kartya7]
+k = [Kartya1, Kartya2, Kartya3, Kartya4, Kartya5, Kartya6, Kartya7, Kartya8, Kartya9]
 tav = 0
 tav2 = 0
 oszl = 0
+old = 1
+font = pygame.font.Font('freesansbold.ttf', 25)
+
 running = True
 
 while running:
